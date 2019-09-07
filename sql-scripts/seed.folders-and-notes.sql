53dece701f469c31fded0b1c2956ed11e1013668
Stephen Parker <stephenparkhum@outlook.com>
2019-09-07 11:31:31 -0700

    Adding new seed data

--- sql-scripts/seed.folders-and-notes.sql
+++ sql-scripts/seed.folders-and-notes.sql
@@ -6,11 +6,10 @@ VALUES
     ('Technology'),
     ('Software Devs');
 
-INSERT INTO notes (title, content, folder_id) VALUES 
-    ('Black Metal', 'Black metal is awesome, here is a list of good bands', '3'),
-    ('Death Metal', 'Death metal is awesome, here is a list of good bands', '3'),
-    ('Apple', 'Here is a list of the latest apple news', '4'),
-    ('JavaScript', 'There are a lot of resources for software devs concerning JavaScript. Here is a list of those', '5');
+INSERT INTO notes (title, modified, content, folder_id) VALUES 
+    ('Black Metal', '2016-01-16 12:00:00', 'Black metal is awesome, here is a list of good bands', '3'),
+    ('Death Metal', '2016-01-16 12:00:00', 'Death metal is awesome, here is a list of good bands', '3'),
+    ('Apple', '2016-01-16 12:00:00', 'Here is a list of the latest apple news', '4'),
+    ('JavaScript', '2016-01-16 12:00:00', 'There are a lot of resources for software devs concerning JavaScript. Here is a list of those', '5');
 
 COMMIT;
-
